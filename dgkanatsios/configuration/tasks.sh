ce6467016df8f5d713913e0336747a62720c6fd2
Tymoteusz Frankiewicz <tymoteusz@frankiewicz.com.pl>
2025-09-14 22:09:03 +0200

    as much as possible when beiing sick

--- dgkanatsios/configuration/tasks.sh
+++ dgkanatsios/configuration/tasks.sh
@@ -29,3 +29,24 @@ kubectl create cm anotherone --from-literal=var6=val6 --from-literal=var7=val7
 kubectl create cm cmvolume --from-literal=var8=val8 --from-literal=var9=val9
 # task3.yaml
 kubectl exec -it nginx3 -- bash
+
+# Create the YAML for an nginx pod that runs with the user ID 101. No need to create the pod
+kubectl run nginx --image=nginx --dry-run=client -o=yaml | tee dgkanatsios/configuration/task4.yaml
+# task4.yaml
+
+# Create the YAML for an nginx pod that has the capabilities "NET_ADMIN", "SYS_TIME" added to its single container
+kubectl run nginx --image=nginx --dry-run=client -o=yaml | tee dgkanatsios/configuration/task5.yaml
+# task5.yaml
+
+# Create an nginx pod with requests cpu=100m,memory=256Mi and limits cpu=200m,memory=512Mi
+kubectl run limitrange --image=nginx --dry-run=client -o=yaml | tee dgkanatsios/configuration/task6.yaml
+# task6.yaml
+
+# Create a namespace named limitrange with a LimitRange that limits pod memory to a max of 500Mi and min of 100Mi
+kubectl create ns limitrange --dry-run=client -o=yaml | tee dgkanatsios/configuration/task7.yaml 
+# task7.yaml
+
+# Describe the namespace limitrange
+kubectl describe ns limitrange
+
+# Create an nginx pod that requests 250Mi of memory in the limitrange namespace
